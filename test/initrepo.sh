dbe9a150b6913a8fe56fdf2f3c90716b758eb12b
Adam Mendlik <amendlik@gmail.com>
2018-01-30 17:27:33 -0700

    Ignore temporary script in test Git repo

--- test/initrepo.sh
+++ test/initrepo.sh
@@ -2,7 +2,8 @@
 if ! git rev-parse --git-dir > /dev/null 2>&1; then
   git init
   git config user.email "author@example.com"
-  git config  user.name "A U Thor"
+  git config user.name "A U Thor"
+  echo '__salt.tmp.*' >> .gitignore
   git add .
   git commit -m "Initial commit"
 fi
