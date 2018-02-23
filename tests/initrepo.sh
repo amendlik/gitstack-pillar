aef2b9478b6f6999edc7044273cf5c983ca0458e
Adam Mendlik <amendlik@gmail.com>
2018-02-23 08:29:11 -0700

    Update test repositories on subsequent converge operations

--- tests/initrepo.sh
+++ tests/initrepo.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env sh
-if ! git rev-parse --git-dir > /dev/null 2>&1; then
+if git rev-parse --git-dir > /dev/null 2>&1
+then
+  git add --all .
+  git diff --cached --exit-code || git commit -m "Commit at $(date --utc --iso-8601=second)"
+else
   git init
   git config user.email "author@example.com"
   git config user.name "A U Thor"
