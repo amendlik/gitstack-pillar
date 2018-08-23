0733d2612aad107d327486a214d3b84678def850
Adam Mendlik <amendlik@gmail.com>
2018-08-22 20:31:04 -0600

    Add tests for Jinja rendering

--- tests/suites/multirepo/verify.sh
+++ tests/suites/multirepo/verify.sh
@@ -6,6 +6,7 @@ $(dirname $0)/../checkpillar.sh 'repo_a' 'present' || result=fail
 $(dirname $0)/../checkpillar.sh 'repo_b1' 'present' || result=fail
 $(dirname $0)/../checkpillar.sh 'repo_b2' 'present' || result=fail
 $(dirname $0)/../checkpillar.sh 'reponame' 'repo_b2' || result=fail
+$(dirname $0)/../checkpillar.sh 'template_key' 'test_value' || result=fail
 
 [ $result = fail ] && exit 1
 exit 0
