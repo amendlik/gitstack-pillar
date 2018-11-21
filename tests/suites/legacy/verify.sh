13d68ccee8f9190a471e7ca6910d7408e85b04d9
Adam Mendlik <amendlik@gmail.com>
2018-11-21 09:55:09 -0700

    Print Salt version number when running tests

--- tests/suites/legacy/verify.sh
+++ tests/suites/legacy/verify.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 result=pass
 
+echo 'Testing Salt version' $(sudo salt-call -c /tmp/kitchen/etc/salt --local --output newline_values_only grains.get saltversion)
+
 $(dirname $0)/../checkpillar.sh 'stacktest' 'True' || result=fail
 
 [ $result = fail ] && exit 1
--- tests/suites/multirepo/verify.sh
+++ tests/suites/multirepo/verify.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 result=pass
 
+echo 'Testing Salt version' $(sudo salt-call -c /tmp/kitchen/etc/salt --local --output newline_values_only grains.get saltversion)
+
 $(dirname $0)/../checkpillar.sh 'stacktest' 'True' || result=fail
 $(dirname $0)/../checkpillar.sh 'repo_a' 'present' || result=fail
 $(dirname $0)/../checkpillar.sh 'repo_b1' 'present' || result=fail
